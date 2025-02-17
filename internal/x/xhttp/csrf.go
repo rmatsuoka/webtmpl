8e677535f9b0cf150b20ce7b62e672a598dd6147
Ryuichi Matsuoka <r.matsuoka@protonmail.com>
2025-02-17 23:18:35 +0900

    csrf

--- internal/x/xhttp/csrf.go
+++ internal/x/xhttp/csrf.go
@@ -1,23 +1,33 @@
 package xhttp
 
 import (
+	"log/slog"
 	"net/http"
 	"slices"
 )
 
 func CSRF(origins []string, h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet || r.Method == http.MethodOptions {
-			h.ServeHTTP(w, r)
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.Method == http.MethodGet || req.Method == http.MethodOptions {
+			h.ServeHTTP(w, req)
 			return
 		}
 
-		origin := r.Header.Get("Origin")
+		origin := req.Header.Get(HeaderOrigin)
+		if origin == "" {
+			http.Error(w, "No Origin header", http.StatusBadRequest)
+			return
+		}
 		if !slices.Contains(origins, origin) {
+			slog.ErrorContext(req.Context(), "CSRF attach",
+				"method", req.Method,
+				"path", req.URL.RequestURI(),
+				"remoteAddr", req.RemoteAddr,
+			)
 			http.Error(w, "bad request", http.StatusBadRequest)
 			return
 		}
 
-		h.ServeHTTP(w, r)
+		h.ServeHTTP(w, req)
 	})
 }
