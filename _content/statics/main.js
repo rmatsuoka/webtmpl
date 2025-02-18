04e5a734b63a87a2b2b90e18aef6a7a53cbf6c74
Ryuichi Matsuoka <r.matsuoka@protonmail.com>
2025-02-18 17:37:59 +0900

    es module

--- _content/statics/main.js
+++ _content/statics/main.js
@@ -1,32 +1,30 @@
-"use strict";
-
-(() => {
-  async function getCount() {
-    const res = await fetch("/api/count");
-    const body = await res.json();
-    if (!res.ok) {
-      throw new Error(body["message"]);
-    }
-    return body["count"];
+async function getCount() {
+  const res = await fetch("/api/count");
+  const body = await res.json();
+  if (!res.ok) {
+    throw new Error(body["message"]);
   }
+  return body["count"];
+}
 
-  async function countup() {
-    const res = await fetch("/api/count", {
-      method: "POST",
-    });
-    const body = await res.json();
-    if (!res.ok) {
-      throw new Error(body["message"]);
-    }
-    return body["count"];
+async function countup() {
+  const res = await fetch("/api/count", {
+    method: "POST",
+  });
+  const body = await res.json();
+  if (!res.ok) {
+    throw new Error(body["message"]);
   }
+  return body["count"];
+}
 
-  const count = document.querySelector("#count");
+const count = document.querySelector("#count");
 
-  function setCount(c) {
-    count.textContent = c;
-  }
+function setCount(c) {
+  count.textContent = c;
+}
 
+function main() {
   getCount().then((c) => setCount(c));
 
   const form = document.querySelector("form#counter");
@@ -34,4 +32,6 @@
     e.preventDefault();
     countup().then((c) => setCount(c));
   });
-})();
+}
+
+main();
