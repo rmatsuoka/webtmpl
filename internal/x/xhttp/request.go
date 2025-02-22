8023d76c6d4a852f2d7a7f07452c1ba9a034e585
Ryuichi Matsuoka <r.matsuoka@protonmail.com>
2025-02-22 18:58:17 +0900

    xhttp.NewRequest

--- internal/x/xhttp/request.go
+++ internal/x/xhttp/request.go
@@ -1,6 +1,8 @@
 package xhttp
 
 import (
+	"context"
+	"io"
 	"maps"
 	"net/http"
 )
@@ -11,4 +13,15 @@ func WithHeader(h http.Header) RequestOption {
 	return func(r *http.Request) {
 		maps.Copy(r.Header, h)
 	}
+}
+
+func NewRequest(ctx context.Context, method, url string, body io.Reader, options ...RequestOption) (*http.Request, error) {
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	for _, option := range options {
+		option(req)
+	}
+	return req, nil
 }
