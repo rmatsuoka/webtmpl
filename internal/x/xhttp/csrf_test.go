fe927ad564e0fe98235f072209d9698e687081d0
Ryuichi Matsuoka <r.matsuoka@protonmail.com>
2025-02-22 22:44:33 +0900

    panicHandler

--- internal/x/xhttp/csrf_test.go
+++ internal/x/xhttp/csrf_test.go
@@ -7,64 +7,62 @@ import (
 )
 
 func TestCSRF(t *testing.T) {
-	okHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("ok"))
-	})
-
-	doTest := func(t *testing.T, wantStatusCode int, method, origin string, handler http.Handler) {
+	doTest := func(t *testing.T, method, origin string, handler http.Handler) {
 		t.Helper()
 		req := httptest.NewRequest(method, "/", nil)
 		req.Header.Set(HeaderOrigin, origin)
 		w := httptest.NewRecorder()
 
 		handler.ServeHTTP(w, req)
-		got := w.Result().StatusCode
-		if got != wantStatusCode {
-			t.Errorf("Request(method=%s [Origin: %s]) returns Response.StatusCode = %d, want %d",
-				method, origin, got, wantStatusCode)
-		}
 	}
 
+	okHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+	panicHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		panic("CSRF Attacked!")
+	})
+
 	t.Run("no origins", func(t *testing.T) {
-		handler := CSRF(nil, okHandler)
 		tests := []struct {
-			method         string
-			origin         string
-			wantStatusCode int
+			method  string
+			origin  string
+			handler http.Handler
 		}{
-			{"GET", "https://example.com", 200},
-			{"HEAD", "https://example.com", 200},
-			{"POST", "https://example.com", 200},
-			{"DELETE", "https://example.com", 200},
-			{"GET", "https://other.example.com", 200},
-			{"HEAD", "https://other.example.com", 200},
-			{"POST", "https://attacker.example.com", 400},
-			{"OPTIONS", "https://attacker.example.com", 400},
+			{"GET", "https://example.com", okHandler},
+			{"HEAD", "https://example.com", okHandler},
+			{"POST", "https://example.com", okHandler},
+			{"DELETE", "https://example.com", okHandler},
+			{"GET", "https://other.example.com", okHandler},
+			{"HEAD", "https://other.example.com", okHandler},
+			{"POST", "https://attacker.example.com", panicHandler},
+			{"OPTIONS", "https://attacker.example.com", panicHandler},
 		}
 
 		for _, test := range tests {
-			doTest(t, test.wantStatusCode, test.method, test.origin, handler)
+			handler := CSRF(nil, test.handler)
+			doTest(t, test.method, test.origin, handler)
 		}
 	})
 
 	t.Run("with specific origins", func(t *testing.T) {
-		handler := CSRF([]string{"https://example.com", "https://api.example.com"}, okHandler)
 		tests := []struct {
-			method         string
-			origin         string
-			wantStatusCode int
+			method  string
+			origin  string
+			handler http.Handler
 		}{
-			{"GET", "https://example.com", 200},
-			{"POST", "https://example.com", 200},
-			{"DELETE", "https://api.example.com", 200},
-			{"GET", "https//other.example.com", 200},
-			{"HEAD", "https//other.example.com", 200},
-			{"POST", "https://attcker.example.com", 400},
-			{"OPTIONS", "https://attacker.example.com", 400},
+			{"GET", "https://example.com", okHandler},
+			{"POST", "https://example.com", okHandler},
+			{"DELETE", "https://api.example.com", okHandler},
+			{"GET", "https//other.example.com", okHandler},
+			{"HEAD", "https//other.example.com", okHandler},
+			{"POST", "https://attcker.example.com", panicHandler},
+			{"OPTIONS", "https://attacker.example.com", panicHandler},
 		}
 
 		for _, test := range tests {
-			doTest(t, test.wantStatusCode, test.method, test.origin, handler)
+			handler := CSRF([]string{"https://example.com", "https://api.example.com"}, test.handler)
+			doTest(t, test.method, test.origin, handler)
 		}
 	})
 }
