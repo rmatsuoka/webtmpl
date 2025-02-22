28af1d24f0f2c56729b6e458f8741480c0603f2b
Ryuichi Matsuoka <r.matsuoka@protonmail.com>
2025-02-22 22:13:15 +0900

    csrf

--- internal/api/routing.go
+++ internal/api/routing.go
@@ -4,11 +4,19 @@ import (
 	"iter"
 	"maps"
 	"net/http"
+
+	"github.com/rmatsuoka/webtmpl/internal/env"
+	"github.com/rmatsuoka/webtmpl/internal/x/xhttp"
+	"github.com/rmatsuoka/webtmpl/internal/x/xiter"
 )
 
 func Handlers() iter.Seq2[string, http.Handler] {
-	return maps.All(map[string]http.Handler{
+	handlers := maps.All(map[string]http.Handler{
 		"GET  /api/count": http.HandlerFunc(count),
 		"POST /api/count": http.HandlerFunc(countup),
 	})
+
+	return xiter.Map2(func(p string, h http.Handler) (string, http.Handler) {
+		return p, xhttp.CSRF(env.APP_CSRF_ORIGINS, h)
+	}, handlers)
 }
--- internal/env/env.go
+++ internal/env/env.go
@@ -4,6 +4,7 @@ import (
 	"cmp"
 	"log/slog"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/rmatsuoka/webtmpl/internal/x/must"
@@ -16,10 +17,19 @@ var (
 	APP_LISTEN_ADDR      = envString("APP_LISTEN_ADDR", ":8080")
 	APP_LOG_LEVEL        = envFunc("APP_LOG_LEVEL", slog.LevelInfo, xslog.ParseLevel)
 	APP_SHUTDOWN_TIMEOUT = envFunc("APP_SHUTDOWN_TIMEOUT", time.Second*10, time.ParseDuration)
+	APP_CSRF_ORIGINS     = envStringSlice("APP_CSRF_ORIGINS", nil)
 )
 
 func envString(key string, fallback string) string {
 	return cmp.Or(getenv(key), fallback)
+}
+
+func envStringSlice(key string, fallback []string) []string {
+	v := getenv(key)
+	if v == "" {
+		return fallback
+	}
+	return strings.Split(v, ",")
 }
 
 func envFunc[T any](key string, fallback T, parse func(string) (T, error)) T {
